26c5c52a1823a92a36d90ecc16b74805d90caf68
Ritu <PES2UG23CS486@pesu.pes.edu>
2026-04-16 20:38:58 +0530

    feat: add DamageReportForm DTO with validation for damage reporting

--- src/main/java/com/example/labtrack/dto/DamageReportForm.java
+++ src/main/java/com/example/labtrack/dto/DamageReportForm.java
@@ -5,12 +5,31 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Data Transfer Object (DTO) used for capturing
+ * damage report form input from the user.
+ */
 @Getter
 @Setter
 public class DamageReportForm {
+
+    /**
+     * ID of the equipment being reported as damaged.
+     * This field is required.
+     */
     @NotNull
     private Long equipmentId;
+
+    /**
+     * ID of the associated reservation, if any.
+     * This field is optional.
+     */
     private Long reservationId;
+
+    /**
+     * Description of the damage provided by the user.
+     * This field must not be blank.
+     */
     @NotBlank
     private String description;
 }
